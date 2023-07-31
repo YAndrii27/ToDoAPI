5a22b7a83709b3bb7c75c3938074fa4cbbde30db
YAndrii27 <AndriiYashchishen@protonmail.com>
2023-07-31 20:55:03 +0100

    Added security: true attribute to cookies sending

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -33,8 +33,8 @@ export class UsersController {
       });
       res
       .status(200)
-      .cookie("authToken", token, {maxAge: 4*60*60*1000, httpOnly: true})
-      .cookie("userID", user.id, {maxAge: 4*60*60*1000, httpOnly: true})
+      .cookie("authToken", token, {maxAge: 4*60*60*1000, httpOnly: true, secure: true})
+      .cookie("userID", user.id, {maxAge: 4*60*60*1000, httpOnly: true, secure: true})
       .redirect("/task");
       return;
     }
